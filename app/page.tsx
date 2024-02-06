2d1c954088d75fe3159cb5decc262924be5a0034
Ayush Pandey <aayushp9sm18@gmail.com>
2024-02-07 00:35:26 +0530

    foresight paragraph added

--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,21 @@ import './HomePage.css';
 export default function Home() {
 
   const [showBrackets, setShowBrackets] = useState(false);
+  const [showParagraph, setShowParagraph] = useState(false);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       setShowBrackets(true);
     }, 9000); 
-    return () => clearTimeout(timeout);
+
+    const paragraphTimeout = setTimeout(() => {
+      setShowParagraph(true);
+    },15000);
+
+    return () => {
+      clearTimeout(timeout);
+      clearTimeout(paragraphTimeout)
+    };
   }, []);
 
   return (
@@ -19,7 +28,10 @@ export default function Home() {
       <span className="blvckPixel">BLVCK PIXEL</span>
       <span className="foresightCompany">The Foresight Company</span>
       <div className="text-container">
-        <span className={`foresight ${showBrackets ? 'slide-up' : ''}`}></span>
+        <span className={`foresight ${showBrackets ? 'slide-up' : ''} text-3xl`}></span>
+        {showParagraph && (
+        <p className='paragraph ml-16 text-2xl italic xl:w-2/5'>the ability to judge correctly what is going to happen in future and plan your actions based on this knowledge.</p>
+        )}
       </div>
     </div>
   );
